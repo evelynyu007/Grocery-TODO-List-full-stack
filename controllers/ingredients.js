28026acf4ee7e8cf56705660c5abd27ca181df05
Wenling Yu <yuwenlingevelyn@gmail.com>
2022-06-14 09:20:02 -0400

    tried to use api from usda

--- controllers/ingredients.js
+++ controllers/ingredients.js
@@ -1,5 +1,11 @@
+/*========================================
+        import dependecies
+========================================*/
+
 const MealprepModel = require("../models/mealprep.js");
 const IngredientModel = require("../models/ingredient.js");
+const fetch = require("node-fetch"); /// NOTE: downgrade
+require("dotenv").config();
 
 module.exports = {
   new: newIngredient,
@@ -32,33 +38,60 @@ function newIngredient(req, res) {
   });
 }
 
+async function show(req, res) {
+  const params = {
+    api_key: "nNW70iQZe3o7p7bCCSrrasvQAt6i2g7rtb0Cz7xs",
+    query: "egg",
+    dataType: ["Survey (FNDDS)"],
+    pagesize: 3,
+  };
+
+  const api_url = `https://api.nal.usda.gov/fdc/v1/foods/search?api_key=${encodeURIComponent(
+    params.api_key
+  )}&query=${encodeURIComponent(params.query)}&dataType=${encodeURIComponent(
+    params.dataType
+  )}&pageSize=${encodeURIComponent(params.pagesize)}`;
+  // can async function inside async function??? or should I move it out
+  async function getData() {
+    const response = await fetch(api_url);
+    return await response.json();
+  }
+  // take a look at data
+  getData().then((data) => console.log(data.foods));
+
+  const dataIngredient = await IngredientModel.findById(req.params.id)
+
+    .then((ingredient) => {
+      res.render("ingredients/show", {
+        title: "Ingredient Detail",
+        ingredient,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.json({ err });
+    });
+}
+
 // function show(req, res) {
-//   IngredientModel.findById(req.params.id)
-//     .then((ingredient) => {
+//   // find same id in ingredients id
+//   IngredientMode.findById(req.params.id)
+
+//   MealprepModel.find({
+//     ingredients: { $in: [req.params.id] },
+//   })
+
+//     .populate("dataMeal")
+//     .then((dataMeal) => {
 //       res.render("ingredients/show", {
 //         title: "Ingredient Detail",
 //         ingredient,
+//         dataMeal,
 //       });
 //     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.json({ err });
+//     // send error as json
+//     .catch((error) => {
+//       console.log(error);
+//       res.json({ error });
 //     });
 // }
-
-function show(req, res) {
-  MealprepModel.find(req.params.id)
-    .populate("mealprep")
-    .exec(function (err, ingredient) {
-      MealprepModel.find(
-        { _id: { $nin: ingredient.mealprep } },
-        function (err, mealprep) {
-          res.render("ingredients/show", {
-            title: "Ingredient Detail",
-            ingredient,
-            mealprep,
-          });
-        }
-      );
-    });
-}
